850f769bc6af7cc4cc44fe4c69496df282341081
Mark Mead <itsmarkmead@gmail.com>
2022-09-22 14:44:21 +0100

    Init

--- scripts/rss.js
+++ scripts/rss.js
@@ -2,31 +2,28 @@ const { promises: fs } = require('fs')
 const path = require('path')
 const Feed = require('feed').Feed
 const matter = require('gray-matter')
-const fetch = require('node-fetch')
 const showdown = require('showdown')
 const converter = new showdown.Converter({ tables: 'true' })
 
 async function generate() {
   const rssFeed = new Feed({
     title: 'Mark Mead',
-    description: 'RSS feed for blog posts.',
+    description: 'RSS feed.',
     id: 'https://www.markmead.dev/',
-    link: 'https://www.markmead.dev/feed.xml',
+    link: 'https://www.markmead.dev/rss.xml',
     language: 'en',
     image: 'https://www.markmead.dev/og.png',
     favicon: 'https://www.markmead.dev/favicon.ico',
     feedLinks: {
-      json: 'https://www.markmead.dev/json',
-      atom: 'https://www.markmead.dev/atom',
+      xml: 'https://www.markmead.dev/rss.xml',
+      json: 'https://www.markmead.dev/rss.json',
+      atom: 'https://www.markmead.dev/rss.atom',
     },
     author: {
       name: 'Mark Mead',
       link: 'https://www.markmead.dev/',
     },
   })
-
-  const hyperPosts = await fetch('https://www.hyperui.dev/rss.json')
-  const hyperData = await hyperPosts.json()
 
   const selfPosts = await fs.readdir(path.join(__dirname, '..', 'posts'))
 
@@ -61,29 +58,7 @@ async function generate() {
         category: convertedPostTags,
       })
     })
-  ).then(() => {
-    hyperData.items.map(async (hyperPost) => {
-      const convertedPostTags = hyperPost.tags.map((postTag) => ({
-        name: postTag,
-      }))
-
-      rssFeed.addItem({
-        title: hyperPost.title,
-        id: hyperPost.id,
-        link: hyperPost.url,
-        description: hyperPost.description,
-        content: converter.makeHtml(hyperPost.content_html),
-        author: [
-          {
-            name: hyperPost.author.name,
-            link: hyperPost.author.url,
-          },
-        ],
-        date: new Date(hyperPost.date_modified),
-        category: convertedPostTags,
-      })
-    })
-  })
+  )
 
   await fs.writeFile('./public/rss.xml', rssFeed.rss2())
   await fs.writeFile('./public/rss.json', rssFeed.json1())
