d374c54a135ad3f6ccf233a6d9e2fc4c74191040
Mark Mead <itsmarkmead@gmail.com>
2022-10-29 10:17:16 +0100

    Fix lint

--- pages/uses.jsx
+++ pages/uses.jsx
@@ -1,64 +1,55 @@
-import Head from 'next/head'
-
 export default function Uses() {
   return (
-    <>
-      <Head>
-        <title>Uses | Mark Mead</title>
-        <meta property="og:title" content="Uses | Mark Mead" key="og:title" />
-      </Head>
+    <article className="prose max-w-none prose-slate dark:prose-invert">
+      <h1>Uses</h1>
 
-      <article className="prose max-w-none prose-slate dark:prose-invert">
-        <h1>Uses</h1>
+      <p className="lead">
+        Here is the tack stack that I use. As for hardware I use one item, my
+        MacBook Pro (14-inch, 2021). Last update was on the 15th September,
+        2022.
+      </p>
 
-        <p className="lead">
-          Here's the tack stack that I use. As for hardware I use one item, my
-          MacBook Pro (14-inch, 2021). Last update was on the 15th September,
-          2022.
-        </p>
+      <h2>Current</h2>
 
-        <h2>Current</h2>
+      <ul>
+        <li>Alpine JS</li>
+        <li>Docker (Lando)</li>
+        <li>Figma</li>
+        <li>GetCandy</li>
+        <li>Hyper (ZSH)</li>
+        <li>JavaScript</li>
+        <li>Laravel</li>
+        <li>Livewire</li>
+        <li>MySQL</li>
+        <li>PHP</li>
+        <li>React JS (Next JS)</li>
+        <li>Shopify</li>
+        <li>TablePlus</li>
+        <li>Tailwind CSS</li>
+        <li>Visual Studio Code</li>
+        <li>Vue JS (Nuxt JS)</li>
+      </ul>
 
-        <ul>
-          <li>Alpine JS</li>
-          <li>Docker (Lando)</li>
-          <li>Figma</li>
-          <li>GetCandy</li>
-          <li>Hyper (ZSH)</li>
-          <li>JavaScript</li>
-          <li>Laravel</li>
-          <li>Livewire</li>
-          <li>MySQL</li>
-          <li>PHP</li>
-          <li>React JS (Next JS)</li>
-          <li>Shopify</li>
-          <li>TablePlus</li>
-          <li>Tailwind CSS</li>
-          <li>Visual Studio Code</li>
-          <li>Vue JS (Nuxt JS)</li>
-        </ul>
+      <h2>Previous</h2>
 
-        <h2>Previous</h2>
-
-        <ul>
-          <li>Adobe XD</li>
-          <li>Atom</li>
-          <li>Bootstrap</li>
-          <li>Foundation</li>
-          <li>Heroku</li>
-          <li>iTerm 2</li>
-          <li>jQuery</li>
-          <li>PostgreSQL</li>
-          <li>ResponsivelyApp</li>
-          <li>Ruby on Rails</li>
-          <li>Ruby</li>
-          <li>SASS</li>
-          <li>SCSS</li>
-          <li>Solidus</li>
-          <li>Stimulus JS</li>
-          <li>Sublime 3</li>
-        </ul>
-      </article>
-    </>
+      <ul>
+        <li>Adobe XD</li>
+        <li>Atom</li>
+        <li>Bootstrap</li>
+        <li>Foundation</li>
+        <li>Heroku</li>
+        <li>iTerm 2</li>
+        <li>jQuery</li>
+        <li>PostgreSQL</li>
+        <li>ResponsivelyApp</li>
+        <li>Ruby on Rails</li>
+        <li>Ruby</li>
+        <li>SASS</li>
+        <li>SCSS</li>
+        <li>Solidus</li>
+        <li>Stimulus JS</li>
+        <li>Sublime 3</li>
+      </ul>
+    </article>
   )
 }
