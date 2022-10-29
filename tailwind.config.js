7b4d62c639f0ba84858e9a1c257408f51aabe76d
Mark Mead <itsmarkmead@gmail.com>
2022-10-29 10:15:06 +0100

    Packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 }
