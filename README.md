87335c644aa545df2abc0b9778115bd6e2f47150
Mark Mead <itsmarkmead@gmail.com>
2022-12-27 17:40:19 +0000

    Remove versions

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-# Website
+# Personal Blog
 
-[markmead.dev](https://markmead.dev/).
-
-### Versions
-
-**Aug 2022 - Present**
-
-Next JS.
-
-https://github.com/markmead/markmead.dev
-
-**Dec 2021 - Aug 2022**
-
-Next JS + Nextra.
-
-https://github.com/markmead/nextra.markmead.dev
-
-**Aug 2021 - Dec 2021**
-
-Nuxt JS + Nuxt Content.
-
-https://github.com/markmead/nuxt.markmead.dev
+This is the code for my personal blog that can be found at [markmead.dev](https://markmead.dev/).
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,7 @@
 export default function Footer() {
-  function handleVersion(targetUrl) {
-    window.location = targetUrl
-  }
-
   return (
     <footer>
-      <div className="flex items-center justify-between gap-4">
-        <p className="text-xs">{new Date().getFullYear()} Mark Mead.</p>
-
-        <div className="flex items-center gap-4">
-          <label htmlFor="WebsiteVersion" className="font-mono text-xs">
-            Website Version
-          </label>
-
-          <select
-            id="WebsiteVersion"
-            onChange={(e) => handleVersion(e.target.value)}
-            className="text-sm border-black"
-          >
-            <option>Latest</option>
-            <option value="https://v2.markmead.dev/">v2</option>
-            <option value="https://v1.markmead.dev/">v1</option>
-          </select>
-        </div>
-      </div>
+      <p className="text-xs">{new Date().getFullYear()} Mark Mead.</p>
     </footer>
   )
 }
